2d633606dc05a277279dea64b7158a87b0587407
jungheeeun <jungheeeun272@gmail.com>
2023-08-27 14:05:28 +0900

    isRequired를 사용하여 필수 propTypes 설정

--- react-prac/src/App.js
+++ react-prac/src/App.js
@@ -3,7 +3,7 @@ import MyComponent from "./MyComponent";
 
 
 const App = () => {
-  return <MyComponent name='Heeun'> App.js : 리액트</MyComponent>
+  return <MyComponent name='Heeun' favoritNumber={12}> App.js : 리액트</MyComponent>
 }
 
 export default App;
--- react-prac/src/MyComponent.js
+++ react-prac/src/MyComponent.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const MyComponent = ({ name, children }) => {
+const MyComponent = ({ name, favoritNumber, children }) => {
   // 함수의 파라미터 부분에서도 사용할 수 있다. 
   return (
     <div> 안녕하세요~ 제 이름은 {name}입니다. <br />
-      children 값은 {children}입니다.
+      children 값은 {children}입니다.<br />
+      제가 좋아하는 숫자는 {favoritNumber}입니다.
     </div>
   );
 };
@@ -14,7 +15,8 @@ MyComponent.prototype = {
 };
 
 MyComponent.propTypes = {
-  name: PropTypes.string
+  name: PropTypes.string,
+  favoritNumber: PropTypes.number.isRequired
 };
 //name 값이 문자열이 아닌 다른 값의 형태로 전달되면 경고메시지를 출력한다. 
 export default MyComponent;
