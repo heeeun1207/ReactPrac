4debcdbd2442c0e0c666fbbda118e936a8038391
jungheeeun <jungheeeun272@gmail.com>
2023-08-27 13:44:38 +0900

    함수 컴포넌트에서도 파라미터 부분을 비구조화 할당 문법 사용하기

--- react-prac/src/MyComponent.js
+++ react-prac/src/MyComponent.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
-const MyComponent = props => {
-  // 비구조화 할당 (구조 분해 문법): 
-  // 객체에서 내부 값을 바로 추출하는 방법
-  const { name, children } = props;
+const MyComponent = ({ name, children }) => {
+  // 함수의 파라미터 부분에서도 사용할 수 있다. 
   return (
-    <div>
-      안녕하세요~ 제 이름은 {name}입니다.<br />
-      children 값은 {children}입니다!
+    <div> 안녕하세요~ 제 이름은 {name}입니다. <br />
+      children 값은 {children}입니다.
     </div>
   );
 };
