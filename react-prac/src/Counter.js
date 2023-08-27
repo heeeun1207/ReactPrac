b539b589141f960ac76150d8eb221425ad291ed3
jungheeeun <jungheeeun272@gmail.com>
2023-08-27 18:39:50 +0900

    함수를 인자로 넣어서 숫자를 2씩 증가 시켰다.

--- react-prac/src/Counter.js
+++ react-prac/src/Counter.js
@@ -14,14 +14,18 @@ class Counter extends Component {
         <h2>state 객체 값 추가 - 바뀌지 않는 값 : {fixedNumber} </h2>
         <button
           onClick={() => {
-            const { number, fixedNumber } = this.state;
-            this.setState({
-              number: number + 1, fixedNumber
+            this.setState(prevState => {
+              return {
+                number: prevState.number + 1
+              };
             });
-            this.setState({
-              number: this.state.number + 1
-            });
+            // 위와 아래는 완전히 똑같은 기능을 한다.
+            // 아래 코드는 함수에서 바로 객체를 반환한다는 의미이다.
+            this.setState(prevState => ({
+              number: prevState.number + 1
+            }));
           }}
+        // 숫자가 2씩 증가하는것을 알 수 있다.
         >
           +1
         </button>
