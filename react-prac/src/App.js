d6b88af8a3f4de19ef73afa33ef65eab974fcc52
jungheeeun <jungheeeun272@gmail.com>
2023-08-27 12:47:10 +0900

    컴포넌트 렌더링

--- react-prac/src/App.js
+++ react-prac/src/App.js
@@ -1,11 +1,8 @@
-import { Component } from 'react';
+import MyComponent from "./MyComponent";
 
-// 클래스형 컴포넌트
-class App extends Component {
-  render() {
-    const name = 'react prac - class component !!!';
-    return <div className='react'>{name}</div>;
-  }
+
+const App = () => {
+  return <MyComponent />;
 }
 
 export default App;
