e5283caeb378904491346de4a9e06c3db4d8b40c
jungheeeun <jungheeeun272@gmail.com>
2023-08-27 11:32:12 +0900

    함수 컴포넌트

---
+++ react-prac/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function App() {
+  const name = '리액트 출력';
+  return <div className='react'>{name}</div>
+}
+
+export default App;
+// 컴포넌트를 선언하는 두가지 방식
+// 1. 함수 컴포넌트  2. 클래스형 컴포넌트 
