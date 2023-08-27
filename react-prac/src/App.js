69d60fdc824295e70f4dde60d2b2f8e98062cf85
jungheeeun <jungheeeun272@gmail.com>
2023-08-27 11:37:48 +0900

    클래스형 컴포넌트

--- react-prac/src/App.js
+++ react-prac/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  const name = '리액트 출력';
-  return <div className='react'>{name}</div>
+// 클래스형 컴포넌트
+class App extends Component {
+  render() {
+    const name = 'react prac - class component !!!';
+    return <div className='react'>{name}</div>;
+  }
 }
 
 export default App;
-// 컴포넌트를 선언하는 두가지 방식
-// 1. 함수 컴포넌트  2. 클래스형 컴포넌트 
