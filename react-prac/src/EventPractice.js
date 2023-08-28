94d41624fac584c3d787fc74d7fd45da62668658
jungheeeun <jungheeeun272@gmail.com>
2023-08-28 17:52:10 +0900

    state에 input 값 담기

--- react-prac/src/EventPractice.js
+++ react-prac/src/EventPractice.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class EventPractice extends Component {
+
+  state = {
+    message: ''
+  } // state 초깃값을 설정
   render() {
     return (
       <div>
@@ -9,10 +13,14 @@ class EventPractice extends Component {
           type='text'
           name='message'
           placeholder='아무거나 입력해 보세요'
+          value={this.state.message}
           onChange={
             (e) => {
-              console.log(e.target.value);
-              //값이 바뀔 때마다 바뀌는 값을 콘솔에 기록한다. 
+              this.setState({
+                message: e.target.value
+                //this.setState 메서드를 호출하여 state를 업데이트 
+                //input의 value 값을 state에 있는 값으로 설정
+              })
             }
           }
         />
