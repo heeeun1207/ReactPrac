6f2d8df880b374ec697e7d53f9629cb2daf7efda
jungheeeun <jungheeeun272@gmail.com>
2023-08-29 23:55:10 +0900

    api

--- api-dev/api-prac/src/App.js
+++ api-dev/api-prac/src/App.js
@@ -1,48 +1,24 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-
-// const App = () => {
-//   const [items, setItems] = useState([]);
-
-//   useEffect(() => {
-//     axios.get('http://localhost:3001/api/items')
-//       .then(response => {
-//         setItems(response.data);
-//       })
-//       .catch(error => {
-//         console.error('Error fetching data:', error);
-//       });
-//   }, []);
-//   const filteredItems = items.filter(item => item.id === 'heeeun'); return (
-//     <div>
-//       <h1>Items</h1>
-//       <ul>
-//         {filteredItems.map(item => (
-//           <li key={item.id}>{item.text}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default App;
 import React, { useState, useEffect } from 'react';
-import jsonData from './data.json';
+import axios from 'axios';
 
 const App = () => {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    const filteredItems = jsonData.filter(item => item.id === 3);
-    setItems(filteredItems);
+    axios.get('http://localhost:3001/api/items')
+      .then(response => {
+        setItems(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
   }, []);
-
-  return (
+  const filteredItems = items.filter(item => item.id === 'heeeun'); return (
     <div>
       <h1>Items</h1>
       <ul>
-        {items.map(item => (
-          <li key={item.id}>{item.name}</li>
+        {filteredItems.map(item => (
+          <li key={item.id}>{item.text}</li>
         ))}
       </ul>
     </div>
@@ -50,3 +26,27 @@ const App = () => {
 };
 
 export default App;
+// import React, { useState, useEffect } from 'react';
+// import jsonData from './data.json';
+
+// const App = () => {
+//   const [items, setItems] = useState([]);
+
+//   useEffect(() => {
+//     const filteredItems = jsonData.filter(item => item.id === 3);
+//     setItems(filteredItems);
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Items</h1>
+//       <ul>
+//         {items.map(item => (
+//           <li key={item.id}>{item.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default App;
