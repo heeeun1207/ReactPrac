d524cdd0847a79d56262507f3945638f580e219c
jungheeeun <jungheeeun272@gmail.com>
2023-09-06 16:27:45 +0900

    더블클릭 이벤트로 특정 원소 제외하기 - filter

--- react-prac/src/IterationSample.js
+++ react-prac/src/IterationSample.js
@@ -22,7 +22,12 @@ const IterationSample = () => {
     setInputText('');// 기존의 input 내용 비우기
   };
 
-  const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
+  const onRemove = id => {
+    const nextNames = names.filter(name => name.id !== id);
+    setNames(nextNames);
+  };
+
+  const namesList = names.map(name => <li key={name.id} onDoubleClick={() => onRemove(name.id)}>{name.text}</li>);
   return (
     <>
       <input value={inputText} onChange={onChange} />
