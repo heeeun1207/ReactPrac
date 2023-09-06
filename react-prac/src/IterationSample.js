2f9f32092d6e64fd68fc849965bfb4ba8a4ace78
jungheeeun <jungheeeun272@gmail.com>
2023-09-06 16:18:51 +0900

    concat 새로운 배열 만들기 : 불변성 유지

--- react-prac/src/IterationSample.js
+++ react-prac/src/IterationSample.js
@@ -1,7 +1,35 @@
+import React, { useState } from 'react';
+
 const IterationSample = () => {
-  const names = ['눈사람', '얼음', '눈', '바람'];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
-  return <ul>{nameList}</ul>;
+  //객체 형태로 이루어진 배열을 만든다. 고유 id 값 생성
+  const [names, setNames] = useState([
+    { id: 1, text: '눈사람' },
+    { id: 2, text: '얼음' },
+    { id: 3, text: '눈' },
+    { id: 4, text: '바람' },
+  ]);
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5); //새로운 항목을 추가할 때 사용할 id
+
+  const onChange = e => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId, // nextId 값을 id 로 설정
+      text: inputText
+    });
+    setNextId(nextId + 1); // nextId 값에 +1 더하기
+    setNames(nextNames); //names 값 업데이트
+    setInputText('');// 기존의 input 내용 비우기
+  };
+
+  const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{namesList}</ul>
+    </>
+  );
 };
 
 export default IterationSample;
