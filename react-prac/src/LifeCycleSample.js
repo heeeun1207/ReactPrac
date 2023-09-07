13c0b5489219f8ac542a589e6d4e149f7921cc53
jungheeeun <jungheeeun272@gmail.com>
2023-09-07 17:37:06 +0900

    클래스형 컴포넌트 - 라이프 사이클 메서드 이해하기

--- react-prac/src/LifeCycleSample.js
+++ react-prac/src/LifeCycleSample.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 
+//! 생명주기(라이프사이클)의 이해와 디버깅 확인 
 class LifeCycleSample extends Component {
   state = {
     number: 0,
     color: null,
   }
-
+  //클래스 내부에서 myRef 변수를 선언한 후, 초기값 null 설정하고 나중에 DOM요소 직접 조작하거나 참조에 사용
   myRef = null; // ref를 설정할 부분 
 
+  //constructor 메소드 : 초기 설정 및 초기 상태 설정 
+  //React 컴포넌트 클래스 Component 를 상속 받으므로 부모 클래스의 생성자를 호출
   constructor(props) {
-    super(props);
+    super(props); //부모 클래스의 생성자를 호출하면서 props 객체를 전달합니다.
     console.log('constructor');
   }
   static getDerivedStateFromProps(nextProps, prevState) {
@@ -20,10 +23,12 @@ class LifeCycleSample extends Component {
     return null;
   }
   //componentDidMount : 컴포넌트가 화면에 나타난 후 호출
+  //! 메서드 내부에서 주로 초기화 작업이나 외부 데이터 로딩 등 화면에 관련된 초기 작업을 수행한다.
   componentDidMount() {
     console.log('componentDidMount');
   }
   //shouldComponentUpdate :  리렌더링 여부를 결정
+  //! 성능 최적화를 위한 렌더링 결정 예시 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('shouldCoponentUpdate', nextProps, nextProps);
     //숫자의 마지막 자리가 4면 리렌더링 하지 않는다.
