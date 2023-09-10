e0f1470d52ab8fed6f578062de6c002d7e491a86
jungheeeun <jungheeeun272@gmail.com>
2023-09-10 12:47:16 +0900

    useReducer 카운터 구현

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,22 +1,7 @@
-import React, { useState } from 'react';
-import Information from './Information';
+import Counter from "./Conuter";
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-
-        {visible ? '숨기기' : '보이기'}
-      </button >
-      <hr />
-      {visible && <Information />}
-    </div>
-  );
-};
+  return <Counter />
+}
 
 export default App;
