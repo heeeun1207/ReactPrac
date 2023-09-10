4bf4f8698993a2ef8493314b80e1ac67678dc1ec
jungheeeun <jungheeeun272@gmai.com>
2023-09-10 22:15:21 +0900

    Next.js 라우팅

---
+++ nextjs-prac/src/app/create/page.js
@@ -0,0 +1,11 @@
+//약속된 파일이름 
+export default function Create (){
+  return(
+    <>Create!!</>
+  )
+}
+//어떻게 동작했을까 ? 
+//create layout.js 파일이 없으므로 부모 폴더인 app에서 
+//layout.js를 찾아서 동작한다.
+
+//create폴더에도 layout 파일이 있다면? 
