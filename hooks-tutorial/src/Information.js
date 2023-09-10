9b201c1a1b6231145e2e0f7e830bde4c12a8c796
jungheeeun <jungheeeun272@gmail.com>
2023-09-10 16:20:20 +0900

    커스텀 훅

--- hooks-tutorial/src/Information.js
+++ hooks-tutorial/src/Information.js
@@ -1,37 +1,29 @@
-import React, { useReducer } from 'react';
+import useInputs from "./useInputs";
 
-function reducer(state, action) {
-  return {
-    ...state,
-    [action.name]: action.value
-  };
-}
 const Information = () => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, onChange] = useInputs({
     name: '',
     nickname: ''
   });
   const { name, nickname } = state;
-  const onChange = e => {
-    dispatch(e.target);
-  };
 
   return (
     <div>
       <div>
-        <input name="name" value={name} onChange={onChange} />
-        <input name="nickname" value={nickname} onChange={onChange} />
+        <input name="name" value={name} onCange={onChange} />
+        <input name="nickname" value={nickname} onCange={onChange} />
       </div>
       <div>
         <div>
-          <b>이름:</b>{name}
+          <b>이름: </b> {name}
         </div>
         <div>
           <b>닉네임 : </b>
           {nickname}
         </div>
       </div>
-    </div >
+    </div>
   );
 };
+
 export default Information;
