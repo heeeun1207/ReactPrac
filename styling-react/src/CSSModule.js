34e73de44b46019d7c85f93666e781efbbf63198
jungheeeun <jungheeeun272@gmail.com>
2023-09-11 12:28:45 +0900

    yarn add classnames

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -2,6 +2,7 @@ import styles from './CSSModule.module.css';
 const CSSModule = () => {
   console.log(styles);
   return (
+    //className={[styles.wrapper, styles.inverted].join(' ')}
     <div className={`${styles.wrapper} ${styles.inverted}`}>
       안녕하세요, 저는 <span className='something'>CSS Module!</span>
     </div>
