c52dd97545a1c3d233544814b7a63dc9c7efd677
jungheeeun <jungheeeun272@gmail.com>
2023-09-12 15:57:14 +0900

    이해하기 -> 스토어의 상태 변경을 감지하여 화면을 업데이트

--- redux-prac/vanilla-redux/index.js
+++ redux-prac/vanilla-redux/index.js
@@ -1,26 +1,31 @@
 import { createStore } from "redux";
 
+// DOM 요소들을 변수에 할당
 const divToggle = document.querySelector('.toggle');
 const counter = document.querySelector('h1');
 const btnIncrease = document.querySelector('#increase');
 const btnDecrease = document.querySelector('#decrease');
 
+// 액션 타입 정의
 const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
 const INCREASE = 'INCREASE';
 const DECREASE = 'DECREASE';
 
+// 액션 생성 함수 정의
 const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
 const increase = difference => ({ type: INCREASE, difference });
 const decrease = () => ({ type: DECREASE });
 
+// 초기 상태 정의
 const initialSate = {
   toggle: false,
   counter: 0
 };
 
+// 리듀서 정의
 function reducer(state = initialSate, action) {
   switch (action.type) {
-    case TOGGLE_SWITCH:
+    case TOGGLE_SWITCH:// 토글 액션 타입
       return {
         ...state,
         toggle: !state.toggle
@@ -46,9 +51,10 @@ divToggle.addEventListener('click', () => {
 
 const store = createStore(reducer); // 스토어 생성
 
-// render 함수 수정
+
 const render = () => {
   const state = store.getState();
+  // 토글 상태에 따라 토글(div)에 'active' 클래스 추가/제거
   if (state.toggle) {
     divToggle.classList.add('active'); // .toggle이 활성화되면 .active 클래스 추가
   } else {
