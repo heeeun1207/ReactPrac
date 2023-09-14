742cea6125e3fade928c1f341895a4e68607b23b
jungheeeun <jungheeeun272@gmail.com>
2023-09-14 17:49:18 +0900

    ul 태그로 Link 구분해놓기

--- router-tutorial/src/pages/Home.js
+++ router-tutorial/src/pages/Home.js
@@ -1,8 +1,24 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div>
       <h1>홈페이지</h1>
       <p>가장 먼저 보여질 페이지</p>
+      <ul>
+          <li>
+          <Link to ="/abuot">소개</Link>
+          </li>
+          <li>
+          <Link to ="/profiles/velopert">velopert의 프로필 정보</Link>
+          </li>
+          <li>
+          <Link to ="/profiles/gildong">gildong 프로필 정보</Link>
+          </li>
+          <li>
+          <Link to ="/profiles/void">존재하지 않는 프로필 정보</Link> 
+          </li>
+      </ul>
     </div>
   );
 };
