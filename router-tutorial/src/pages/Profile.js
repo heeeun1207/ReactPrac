7afbafd58b92d5dc906f3750f169f2283d72b2ef
jungheeeun <jungheeeun272@gmail.com>
2023-09-14 17:42:42 +0900

    오타수정

--- router-tutorial/src/pages/Profile.js
+++ router-tutorial/src/pages/Profile.js
@@ -9,7 +9,7 @@ import {useParams} from 'react-router-dom';
 const data = {
   velopert:{
     name: '정희은',
-    description: '리액트와 친해지는중인 개발자'
+    description: '리액트와 친해지는중인 개발자',
   },
   gildong:{
     name:'홍길동',
