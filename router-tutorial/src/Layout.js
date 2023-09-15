ae9bd051d420cb4ae32804d77c70193c4aefd49e
jungheeeun <jungheeeun272@gmail.com>
2023-09-15 16:36:19 +0900

    useNavigate 라우터 부가기능

--- router-tutorial/src/Layout.js
+++ router-tutorial/src/Layout.js
@@ -1,11 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Layout = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    //이전 페이지로 이동
+    navigate(-1);
+  };
+  
+  const goArticles = () => {
+    // articles 게시판목록으로 이동 
+    navigate('/articles');
+  };
+
   return (
   <div>
     <header style = {{background :'lightgray',padding:16, fontSize :24}}>
-      Header 
+      <button onClick={goBack}>뒤로가기 버튼</button>
+      <button onClick={goArticles}>게시판 목록 버튼</button>
     </header>
+
     <main>
       <Outlet/>
     </main>
