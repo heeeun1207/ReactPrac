a6e867825ae4b2def21b6eb2bf20ee9dbcb22295
jungheeeun <jungheeeun272@gmail.com>
2023-09-15 16:24:17 +0900

    헤더 렌더링

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -4,19 +4,22 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Article from './pages/Article';
 import Articles from './pages/Articles';
+import Layout from './Layout';
 
 function App() {
   return (
     <Routes>
+      <Route element={<Layout />}>
       <Route path="/" element={<Home />}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/profiles/:username" element={<Profile />}/>
-
+      </Route>
       {/* /articles 경로에 Articles 컴포넌트 내용인 목록페이지 렌더링 */}
       <Route path="/articles" element={<Articles />}>
         {/* /articles/1 게시글을 들어가면  Article 컴포넌트의 <h2>태그와 articles목록이 같이 렌더링  */}
         <Route path=":id" element={<Article />}/>
         </Route>
+
     </Routes>
   );
 }
