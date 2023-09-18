6fa6e820d4e6cd9424bf50cf5ecdc6425aadcdfb
jungheeeun <jungheeeun272@gmail.com>
2023-09-18 15:14:18 +0900

    async 적용하기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -3,13 +3,15 @@ import axios from 'axios';
 
 const App =() =>{
   const [data,setData ] =useState(null);
-  const onClick = () => {
-    // 실제 URL에서 제공하는 가짜 API를 호출하고 이에 응답을 하는것을 보여준다. 
-    // axios.get 함수 사용 -> 파라미터로 전달된 주소에 GET 요청을 해 준다. 
-    // 이에 대한 결과는 .then을 통해 비동기적으로 확인할 수 있다.
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response =>{
+  // async 적용해보기 
+  const onClick = async () => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1',
+      );
       setData(response.data);
-    });
+    } catch (e){
+      console.log(e);
+    }
   };
   return(
     <div>
