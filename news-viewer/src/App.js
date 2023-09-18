a72ac728bf106cca49787b531b987f07a8e7b6fe
jungheeeun <jungheeeun272@gmail.com>
2023-09-18 17:35:49 +0900

    category 값을 업데이트할 onSelect 함수 생성

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,14 +1,19 @@
+//* 1. category 상태 관리하기 useState 
+//* 2. category 값을 업데이트할 onSelect 함수 만들기 
+import { useState, useCallback } from "react";
 import NeswList from "./components/NewsList";
 import Categories from "./components/Categories";
 
 const App = () =>{
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback (category => setCategory(category), []);
+
   return (
     <>
-      <Categories />
-      <NeswList />
+      <Categories category={category} onSelect={onSelect} />
+      <NeswList category={category}/>
     </>
   )
-
 };
 
 export default App;
