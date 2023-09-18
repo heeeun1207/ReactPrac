45d3e570da8af6426c731bfcecc9a08377bc18ef
jungheeeun <jungheeeun272@gmail.com>
2023-09-18 16:11:13 +0900

    NeswList 컴포넌트 렌더링

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,26 +1,7 @@
-import { useState } from "react";
-import axios from 'axios';
+import NeswList from "./components/NewsList";
 
-const App =() =>{
-  const [data,setData ] =useState(null);
-  // async 적용해보기 
-  const onClick = async () => {
-    try {
-      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=API_KEY',
-      );
-      setData(response.data);
-    } catch (e){
-      console.log(e);
-    }
-  };
-  return(
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data,null,2)} readOnly={true} />}
-    </div>
-  );
+const App = () =>{
+  return <NeswList />;
 };
 
 export default App;
