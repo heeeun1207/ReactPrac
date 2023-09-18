61ef679851d734f52b4c7a89d70115b306ffdb8d
jungheeeun <jungheeeun272@gmail.com>
2023-09-18 15:55:34 +0900

    컴포넌트 생성 : 각 필드 내용 중 사용할 데이터 정하기

---
+++ news-viewer/src/components/NewsItem.js
@@ -0,0 +1,19 @@
+// {
+//   "status": "ok",
+//   "totalResults": 34,
+//   "articles": [
+//     {
+//       "source": {
+//         "id": null,
+//         "name": "Hani.co.kr"
+//       },
+//       "author": null,
+//       "title": "어느 중간관리자들의 '갑질 복마전'…동성 성기 만지고, '머리 안 자르겠다' 녹음 강요까지 - 한겨레",
+//       "description": "욕설·폭력·성희롱에 ‘테스트테크’ 노동부 특별감독과태료 1천만원 부과하고 가해자 징계 요구",
+//       "url": "https://www.hani.co.kr/arti/society/labor/1108867.html",
+//       "urlToImage": "https://flexible.img.hani.co.kr/flexible/normal/708/531/imgdb/original/2023/0917/20230917501445.jpg",
+//       "publishedAt": "2023-09-17T06:35:38Z",
+//       "content": "2023-09-17 15:352023-09-17 16:10"
+//     },
+//* 각 뉴스 데이터가 지니고 있는 정보의 JSON 객체이다. 
+//* 그중에서 title,desciption,url,urlToImage 필드를 컴포넌트에 나타내보자 
