71736e681b045bcb8c283400852ed644eb9bc8ec
jungheeeun <jungheeeun272@gmail.com>
2023-09-19 15:43:11 +0900

    색상 컴포넌트 준비

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -3,10 +3,13 @@ import { ColorProvider } from "./contexts/color";
 // Provider 사용해서 Context의 value를 변경할 수 있다. 
 // Provider를 사용할 때 value 값을 반드시 명시해야 오류가 나지 않음
 //    <ColorContext.Provider> 오류 발생 
+import SelectColors from "./components/SelectColors";
+
 const  App = () => {
   return (
     <ColorProvider>
       <div>
+        <SelectColors />
         <ColorBox />
       </div>
     </ColorProvider>
