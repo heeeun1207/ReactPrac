f801ed6e57be41b3638fb5bf07b1d903f0e773ba
jungheeeun <jungheeeun272@gmail.com>
2023-09-19 14:32:42 +0900

    Provider 사용해서 value color 변경

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,10 +1,13 @@
 import ColorBox from "./components/ColorBox";
-
+import ColorContext from "./contexts/color";
+// Provider 사용해서 Context의 value를 변경할 수 있다. 
 const  App = () => {
   return (
-    <div>
-      <ColorBox /> 
-    </div>
+    <ColorContext.Provider value={{color: 'pink'}}>
+      <div>
+        <ColorBox /> 
+      </div>
+    </ColorContext.Provider>
   );
 };
 
