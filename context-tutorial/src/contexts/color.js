b40feb78f1d49919adbcb033e3627b5f32a41796
jungheeeun <jungheeeun272@gmail.com>
2023-09-19 14:30:17 +0900

    렌더링, 오타수정

--- context-tutorial/src/contexts/color.js
+++ context-tutorial/src/contexts/color.js
@@ -1,5 +1,5 @@
 import {createContext } from 'react';
 
-const ColorContext = createContext({color : 'balck'});
+const ColorContext = createContext({color : 'yellow'});
 
 export default ColorContext;
