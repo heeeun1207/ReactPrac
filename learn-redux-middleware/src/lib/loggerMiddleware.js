2fa0cb0de86e4b4d6f81bb1f3a9e2cc1d0436b81
jungheeeun <jungheeeun272@gmail.com>
2023-09-20 12:12:39 +0900

    미들웨어 직접 만들기

---
+++ learn-redux-middleware/src/lib/loggerMiddleware.js
@@ -0,0 +1,15 @@
+const loggerMiddleware = store => next => action => {
+  //미들웨어 기본 구조 
+};
+
+export default loggerMiddleware;
+
+
+
+// const loggerMiddleware = function loggerMiddleware(store){
+//   return function(next){
+//     return function(action) {
+    
+//     };
+//   };
+// };
