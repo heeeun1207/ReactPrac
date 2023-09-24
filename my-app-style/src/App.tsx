d85727be27f8e5bf8c10e3229aadaabb9b1f9d78
jungheeeun <jungheeeun272@gmail.com>
2023-09-24 17:48:43 +0900

    물리적인 css파일 대체

--- my-app-style/src/App.tsx
+++ my-app-style/src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  text-align: center;
+`;
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    // <div className="App">
+    // <header className="App-header">
+
+    <Container>
+      <Header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,8 +36,11 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </Header>
+    </Container>
+
+    // </header>
+    // </div>
   );
 }
 
