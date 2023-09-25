ffae6446283b8093a3782474cd2d3dabf25ad3e6
jungheeeun <jungheeeun272@gmail.com>
2023-09-25 21:26:09 +0900

    서브페이지

--- butter-project/src/App.js
+++ butter-project/src/App.js
@@ -6,8 +6,8 @@ import image3 from './assets/images/image3.png';
 import image4 from './assets/images/image4.png';
 import LoginPopup from './components/LoginPopup';
 import HamburgerIcon from './assets/images/Hamburger.png';
-
-// signUp 함수를 import
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import SubPage from './components/SubPage';
 import { signUp } from './api/userApi';
 
 class App extends Component {
@@ -28,7 +28,7 @@ class App extends Component {
   componentWillUnmount() {
     clearInterval(this.interval);
   }
-
+  
   changeImage = () => {
     this.setState((prevState) => ({
       currentImageIndex: (prevState.currentImageIndex + 1) % this.state.images.length,
@@ -42,69 +42,66 @@ class App extends Component {
   };
 
   handleSignUpClick = async () => {
-    // 회원 가입 로직 구현
     try {
       const userData = {
-        userId: '사용자 아이디', // 사용자 아이디 입력
-        password: '비밀번호', // 비밀번호 입력
-        userName: '사용자 이름', // 사용자 이름 입력
-        email: '이메일 주소', // 이메일 주소 입력
-        mobile: '휴대폰 번호', // 휴대폰 번호 입력
+        userId: '사용자 아이디',
+        password: '비밀번호',
+        userName: '사용자 이름',
+        email: '이메일 주소',
+        mobile: '휴대폰 번호',
       };
-      const response = await signUp(userData); 
+      const response = await signUp(userData);
       console.log('회원 가입 성공:', response);
-      // 회원 가입 성공 시 필요한 동작 수행
     } catch (error) {
       console.error('회원 가입 오류:', error.message);
-      // 회원 가입 실패 시 에러 처리
     }
   };
+
   render() {
     const { currentImageIndex, images, isLoggedIn } = this.state;
     const currentImage = images[currentImageIndex];
 
     return (
-      <div className="App" style={{ position: 'relative' }}>
-        <img
-          src={currentImage}
-          alt={`car ${currentImageIndex + 1}`}
-          className="background-image" 
-        />
-
-        <button
-          onClick={this.toggleLogin}
-          className="login-button" 
-        >
+      <Router>
+        <div className="App" style={{ position: 'relative' }}>
           <img
-            src={HamburgerIcon}
-            alt="Hamburger"
-            className="hamburger-icon" 
+            src={currentImage}
+            alt={`car ${currentImageIndex + 1}`}
+            className="background-image"
           />
-        </button>
 
-        <h1 className="app-title">BUTTER</h1>
+          <Link to="/subpage">
+            <button className="login-button">
+              <img
+                src={HamburgerIcon}
+                alt="Hamburger"
+                className="hamburger-icon"
+              />
+            </button>
+          </Link>
 
-        <h2
-          className="reservation"
-          onClick={this.toggleLogin}
-        >
-          RESERVATION
-        </h2>
+          <h1 className="app-title">BUTTER</h1>
 
-        {/* 회원 가입 버튼 */}
-        {!isLoggedIn && (
-          <button
-            onClick={this.handleSignUpClick} // 회원 가입 버튼 클릭 이벤트 핸들러 추가
-            className="signup-button"
-          >
-            Sign up
-          </button>
-        )}
+          <h2 className="reservation" onClick={this.toggleLogin}>
+            RESERVATION
+          </h2>
 
-        {/* 로그인 팝업 */}
-        {isLoggedIn && <LoginPopup />}
+          {!isLoggedIn && (
+            <button
+              onClick={this.handleSignUpClick}
+              className="signup-button"
+            >
+              Sign up
+            </button>
+          )}
 
-      </div>
+          {isLoggedIn && <LoginPopup />}
+
+          <Routes>
+            <Route path="/subpage" element={<SubPage />} />
+          </Routes>
+        </div>
+      </Router>
     );
   }
 }
---
+++ butter-project/src/components/SubPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SubPage() {
+  return (
+    <div>
+      <h2>This is a SubPage</h2>
+      <p>Additional content for your SubPage can be added here.</p>
+    </div>
+  );
+}
+
+export default SubPage;
