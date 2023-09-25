f11c24dc1b0c0ed1a7222124c3d4d9bb444412f6
jungheeeun <jungheeeun272@gmail.com>
2023-09-25 18:16:31 +0900

    회원가입 글자 왼쪽으로 이동필요, 아래 Join us추가

--- butter-project/src/components/SignUpComponent.js
+++ butter-project/src/components/SignUpComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { signUp } from '../api/userApi';
+import './SignUpComponent.css'; // SignUpComponent.css 파일을 import
 
 class SignUpComponent extends Component {
   constructor(props) {
@@ -70,63 +71,73 @@ class SignUpComponent extends Component {
 
   render() {
     return (
-      <div>
-        <h2>회원 가입</h2>
-        <div>
+      <div className="login-popup"> 
+        <h2 className="login-title">회원 가입</h2> 
+        <div className="input-container"> 
           <label htmlFor="userId">사용자 아이디:</label>
           <input
             type="text"
             id="userId"
             value={this.state.userId}
             onChange={this.handleUserIdChange}
+            className="input-field" 
           />
         </div>
-        <div>
+        <div className="input-container"> 
           <label htmlFor="password">비밀번호:</label>
           <input
             type="password"
             id="password"
             value={this.state.password}
             onChange={this.handlePasswordChange}
+            className="input-field" 
           />
         </div>
-        <div>
+        <div className="input-container"> 
           <label htmlFor="Re-Enter">비밀번호 확인:</label>
           <input
             type="password"
             id="Re-Enter"
             value={this.state['Re-Enter']}
             onChange={this.handleConfirmPasswordChange}
+            className="input-field" 
           />
         </div>
-        <div>
+        <div className="input-container"> 
           <label htmlFor="userName">사용자 이름:</label>
           <input
             type="text"
             id="userName"
             value={this.state.userName}
             onChange={this.handleUserNameChange}
+            className="input-field" 
           />
         </div>
-        <div>
+        <div className="input-container"> 
           <label htmlFor="email">이메일:</label>
           <input
             type="email"
             id="email"
             value={this.state.email}
             onChange={this.handleEmailChange}
+            className="input-field" 
           />
         </div>
-        <div>
+        <div className="input-container"> 
           <label htmlFor="mobile">휴대폰 번호:</label>
           <input
             type="tel"
             id="mobile"
             value={this.state.mobile}
             onChange={this.handleMobileChange}
+            className="input-field" 
           />
         </div>
-        <button onClick={this.handleSignUp}>회원 가입</button>
+        <div className="sign-in-up"> 
+          <span>Sign in</span>
+          <span className="separator">|</span>
+          <span onClick={this.handleSignUp}>Sign up</span>
+        </div>
       </div>
     );
   }
