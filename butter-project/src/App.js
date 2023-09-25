cabac9ef0f484876ab4c2ceae1c82dd22bafa032
jungheeeun <jungheeeun272@gmail.com>
2023-09-25 15:26:47 +0900

    햄버거버튼

--- butter-project/src/App.js
+++ butter-project/src/App.js
@@ -4,7 +4,8 @@ import image1 from './assets/images/image1.png';
 import image2 from './assets/images/image2.png';
 import image3 from './assets/images/image3.png';
 import image4 from './assets/images/image4.png';
-import LoginPopup from './components/LoginPopup'; 
+import LoginPopup from './components/LoginPopup';
+import HamburgerIcon from './assets/images/Hamburger.png';
 
 class App extends Component {
   constructor(props) {
@@ -43,14 +44,70 @@ class App extends Component {
 
     return (
       <div className="App" style={{ position: 'relative' }}>
-        <img src={currentImage} alt={`car ${currentImageIndex + 1}`} />
+        <img
+          src={currentImage}
+          alt={`car ${currentImageIndex + 1}`}
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100vw',
+            height: '100vh',
+            objectFit: 'cover',
+          }}
+        />
 
-        <h1 style={{ position: 'absolute', top: 0, left: '50%', transform: 'translateX(-50%)', zIndex: 1, color: 'white' }}>BUTTER</h1>
+        <button
+          onClick={this.toggleLogin}
+          style={{
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            marginTop: '60px',
+            marginRight: '41px',
+            background: 'transparent',
+            border: 'none',
+            cursor: 'pointer',
+          }}
+        >
+          <img
+            src={HamburgerIcon}
+            alt="Hamburger"
+            style={{ width: '54px', height: '37px' }}
+          />
+        </button>
+
+        <h1
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: '50%',
+            transform: 'translateX(-50%)',
+            zIndex: 1,
+            color: 'white',
+            marginTop: '44px',
+            marginLeft: '-100px',
+            fontSize: '72px',
+          }}
+        >
+          BUTTER
+        </h1>
 
         <h2
-          style={{ position: 'absolute', top: 0, left: 0, zIndex: 1, color: 'white', cursor: 'pointer' }}
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            zIndex: 1,
+            color: 'white',
+            cursor: 'pointer',
+            marginTop: '60px',
+            marginLeft: '41px',
+          }}
           onClick={this.toggleLogin}
-        >RESERVATION</h2>
+        >
+          RESERVATION
+        </h2>
 
         {isLoggedIn && <LoginPopup />}
       </div>
