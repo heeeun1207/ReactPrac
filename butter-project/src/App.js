5a8147e13c9a7da1f8bc3feab58f269752996e0e
jungheeeun <jungheeeun272@gmail.com>
2023-09-26 16:52:09 +0900

    SubPage 두번째 카테고리 페이지

--- butter-project/src/App.js
+++ butter-project/src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Route path="/" element={<HomeContent />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/subpage" element={<SubPage />} />
+        {/* SubPage 두번째 카테고리 페이지  */}
       </Routes>
     </BrowserRouter>
   );
