d316c4b0dffea64e147db9433dde938a1d2627ef
jungheeeun <jungheeeun272@gmail.com>
2023-09-26 16:32:13 +0900

    링크추가

--- butter-project/src/components/SubPage.js
+++ butter-project/src/components/SubPage.js
@@ -17,7 +17,7 @@ function SubPage() {
         <div className="title">HIGH-END CAR STORE</div>
         <div className="contents">하이엔드 직수입, 구매부터 튜닝까지 고객의 요구에 맞춘 최상의 서비스를 제공합니다.</div>
       </div>
-      {["High-end Car","MAINTENANCE", "TUNING CAR", "BODY PAINT"].map((item, index) => (
+      {["High-end Car", "MAINTENANCE", "TUNING CAR", "BODY PAINT"].map((item, index) => (
         <div
           key={index}
           className={`subpage-item ${index === selectedItem ? 'selected' : ''}`}
@@ -28,6 +28,11 @@ function SubPage() {
       ))}
 
       <img src={image5} alt="Image5" className='image5' />
+
+      {/* VIEW MORE 링크 추가 */}
+      <a href="https://github.com/heeeun1207" target="_blank" rel="noopener noreferrer" className="view-more">
+        VIEW MORE
+      </a>
     </div>
   );
 }
